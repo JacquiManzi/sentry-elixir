f5a56868bceee77b512445066b50bd111ad4bc25
Jacqui Manzi <jacquimanzi@gmail.com>
2019-09-17 15:59:15 -0700

    testing

--- lib/sentry/phoenix_endpoint.ex
+++ lib/sentry/phoenix_endpoint.ex
@@ -13,40 +13,40 @@ defmodule Sentry.Phoenix.Endpoint do
         use Sentry.Phoenix.Endpoint
 
   """
-  defmacro __using__(_opts) do
-    quote do
-      @before_compile Sentry.Phoenix.Endpoint
-    end
-  end
-
-  defmacro __before_compile__(_) do
-    quote do
-      defoverridable call: 2
-
-      def call(conn, opts) do
-        try do
-          super(conn, opts)
-        catch
-          kind, %Phoenix.Router.NoRouteError{} ->
-            :erlang.raise(kind, %Phoenix.Router.NoRouteError{}, __STACKTRACE__)
-
-          kind, reason ->
-            stacktrace = __STACKTRACE__
-            request = Sentry.Plug.build_request_interface_data(conn, [])
-            exception = Exception.normalize(kind, reason, stacktrace)
-
-            _ =
-              Sentry.capture_exception(
-                exception,
-                stacktrace: stacktrace,
-                request: request,
-                event_source: :endpoint,
-                error_type: kind
-              )
-
-            :erlang.raise(kind, reason, stacktrace)
-        end
-      end
-    end
-  end
+#  defmacro __using__(_opts) do
+#    quote do
+#      @before_compile Sentry.Phoenix.Endpoint
+#    end
+#  end
+#
+#  defmacro __before_compile__(_) do
+#    quote do
+#      defoverridable call: 2
+#
+#      def call(conn, opts) do
+#        try do
+#          super(conn, opts)
+#        catch
+#          kind, %Phoenix.Router.NoRouteError{} ->
+#            :erlang.raise(kind, %Phoenix.Router.NoRouteError{}, __STACKTRACE__)
+#
+#          kind, reason ->
+#            stacktrace = __STACKTRACE__
+#            request = Sentry.Plug.build_request_interface_data(conn, [])
+#            exception = Exception.normalize(kind, reason, stacktrace)
+#
+#            _ =
+#              Sentry.capture_exception(
+#                exception,
+#                stacktrace: stacktrace,
+#                request: request,
+#                event_source: :endpoint,
+#                error_type: kind
+#              )
+#
+#            :erlang.raise(kind, reason, stacktrace)
+#        end
+#      end
+#    end
+#  end
 end
